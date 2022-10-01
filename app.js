ecb93d41b8ebe28b14443c05e05ab4675fdb7eff
icyhot12 <rmierniczek1@gmail.com>
2022-10-01 19:30:45 +0200

    start logic

--- app.js
+++ app.js
@@ -8,3 +8,36 @@ function setTextHeight() {
     let imageHeight = auctionImage.offsetHeight;
     description.style.height = imageHeight;
 }
+
+//time logic
+
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+const weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+
+const giveaway = document.querySelector('.auction-title-small');
+const deadline = document.querySelector('.deadline-container');
+const items = document.querySelectorAll('.box h4');
+
+let futureDate = new Date(2020,4,24,11,30,0);
+
