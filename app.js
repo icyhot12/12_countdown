c61c715a0eaf9fe0133953471c80dcb02cb9e1d9
icyhot12 <rmierniczek1@gmail.com>
2022-10-02 11:28:51 +0200

    add timer logic

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const months = [
     "November",
     "December",
 ];
-const weekdays = [
+const weekdaysArray = [
     "Sunday",
     "Monday",
     "Tuesday",
@@ -39,5 +39,55 @@ const giveaway = document.querySelector('.auction-title-small');
 const deadline = document.querySelector('.deadline-container');
 const items = document.querySelectorAll('.box h4');
 
-let futureDate = new Date(2020,4,24,11,30,0);
+let futureDate = new Date(2022, 9, 2, 11, 16, 30);
 
+const year = futureDate.getFullYear();
+let month = futureDate.getMonth();
+let dateNumber = futureDate.getDate();
+const hours = futureDate.getHours();
+let minutes = futureDate.getMinutes();
+let weekDayNumber = futureDate.getDay();
+
+monthText = months[month];
+
+let weekDayFinal = weekdaysArray[weekDayNumber];
+
+function numberFormat(value) {
+    if (value < 10) {
+        return (value = '0' + value);
+    }
+    return value
+};
+
+giveaway.textContent = `giveaway ends on ${weekDayFinal}, ${numberFormat(dateNumber)} ${monthText} ${year} ${hours}:${numberFormat(minutes)}`;
+
+const oneDay = 24 * 60 * 60 * 1000;
+const oneHour = 60 * 60 * 1000;
+const oneMinute = 60 * 1000;
+const oneSecond = 1000;
+
+function getTimeDif() {
+    let currentDate = new Date();
+    let dateDif = futureDate - currentDate;
+
+    let remainDays = Math.floor(dateDif / oneDay);
+    let remainHours = Math.floor(dateDif / oneHour) - remainDays * 24;
+    let remainMinutes = Math.floor(dateDif / oneMinute) - remainDays * 24 * 60 - remainHours * 60;
+    let remainSeconds = Math.floor(dateDif / oneSecond) - remainDays * 24 * 60 * 60 - remainHours * 60 * 60 - remainMinutes * 60;
+
+    const values = [remainDays, remainHours, remainMinutes, remainSeconds];
+
+    // function format 
+
+    items.forEach((item, index) => {
+        item.innerHTML = numberFormat(values[index]);
+    })
+
+    if(dateDif <0) {
+        clearInterval(countdown)
+    }
+
+};
+
+let countdown = setInterval(getTimeDif, 1000);
+getTimeDif();
